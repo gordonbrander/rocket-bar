5f44225e8e658a1b78a2470babf0b0978591eacb
Gordon Brander <marlfoskr@gmail.com>
2013-01-23 19:09:14 -0700

    Add MPL license headers

--- kicks.js
+++ kicks.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Convert a method func that uses a `this` context into a function that takes
 // the context object as the first parameter.
 function lambda(method) {
