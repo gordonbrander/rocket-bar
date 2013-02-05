ee86a27696c5169bdd081e7170f7fa414bcd03ee
Gordon Brander <marlfoskr@gmail.com>
2013-02-04 17:58:23 -0800

    Only render suggestions if they are different
    We don't want to suggest what's already in the box.

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var field = require('oops/field');
 var query = require('oops/query');
 var dropRepeats = require('transducer/drop-repeats');
 var take = require('reducers/take');
+var takeWhile = require('reducers/take-while');
 var into = require('reducers/into');
 var when = require('eventual/when');
 
@@ -325,26 +326,28 @@ var completionValuesOverTime = map(completionTitleElementsOverTime, function (el
 });
 
 // Merge clicked suggested values stream and actionBar values stream.
-var searchQuery = merge([completionValuesOverTime, actionBarValuesOverTime]);
-
 // Create signal representing query terms entered into action bar,
 // also repeats in `searchQuery` are dropped to avoid more work
 // down the flow.
-var searchTerms = map(dropRepeats(searchQuery), function(query) {
-  return query.split(/\s+/);
-});
+var searchQueriesOverTime = dropRepeats(merge([
+  completionValuesOverTime,
+  actionBarValuesOverTime
+]));
 
 // Continues signal representing search results for the entered query.
 // special `SOQ` value is used at as delimiter to indicate results for
 // new query. This can be used by writer to flush previous inputs and
 // start writing now ones.
-var resultSetsOverTime = map(searchTerms, function(terms) {
+var resultSetsOverTime = map(searchQueriesOverTime, function(query) {
+  var terms = query.split(/\s+/);
+
   if (!terms.length || !terms[0]) return SOQ;
 
   // Search noun matches. Accesses closure variable NOUNS.
   var nounMatches = grepNounMatches(terms, NOUNS);
 
   return {
+    query: query,
     suggestions: nounMatches,
     actions: merge([ searchWithVerb(terms), expandNounMatchesToActions(nounMatches, actionsByType) ])
   };
@@ -370,7 +373,8 @@ var renderType = {
 };
 
 var actionBarElement = document.getElementById('action-bar');
-print(completionValuesOverTime);
+
+// Update 
 fold(completionValuesOverTime, function (value) {
   actionBarElement.value = value;
 });
@@ -400,12 +404,22 @@ fold(resultSetsOverTime, function (resultSet) {
   // Take the first 100 results and use as the sample size for sorting by score..
   var top100Suggestions = sortFirstX(suggestions, 100, compareSuggestions);
   var cappedSuggestions = take(top100Suggestions, 4);
-
+  
+  // Transform the limited set of suggestions into strings.
   var suggestionTitles = map(cappedSuggestions, function (suggestion) {
     return suggestion[0].noun.serialized;
   });
 
-  var eventualSuggestionsHtml = fold(suggestionTitles, function (title, html) {
+  // Filter out suggestions that are equivalent to the terms already in the
+  // action bar.
+  var validSuggestionTitles = filter(suggestionTitles, function (title) {
+    return title.toLowerCase() !== resultSet.query.toLowerCase();
+  });
+
+  // Create an HTML string for each suggestion entry.
+  // If there are no suggestions we'll end up reducing to an empty string, and
+  // hence no suggestions are rendered. Perfect!
+  var eventualSuggestionsHtml = fold(validSuggestionTitles, function (title, html) {
     return html + '<li class="action-completion">' + 
       '<span class="title">' +
       title +
@@ -413,6 +427,7 @@ fold(resultSetsOverTime, function (resultSet) {
       '</li>'
   }, '');
 
+  // Render the HTML for suggestions.
   fold(eventualSuggestionsHtml, function (html) {
     suggestionsContainer.innerHTML = html;
   });
