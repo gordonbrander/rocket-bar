103311beee569ca092ff2fdcc1a5b62f5a5e8d16
Gordon Brander <marlfoskr@gmail.com>
2013-02-13 10:47:00 -0700

    Match query using logical AND regex
    Allows words to appear in any order, allowing matches for:
    
    * ada wong
    * wong ada
    * a wong
    * wong a
    * Ada what is the
    
    Etc...

--- index.js
+++ index.js
@@ -248,6 +248,16 @@ function isLongerThan(reducible, length) {
   });
 }
 
+function foldWordsToLogicalAndPatternString(word, accumulated) {
+  // Express logical AND using lookaheads.
+  // See <http://ocpsoft.org/tutorials/regular-expressions/and-in-regex/>
+  return accumulated + '(?=.*' + word + ')';
+}
+
+function convertStringToCaseInsensitivePattern(string) {
+    return Pattern(string, 'i');
+}
+
 function convertQueryStringToPattern(queryString) {
   // Take a string representing search terms, escape it, prepare it and
   // turn it into a liberally matching pattern suitable for grep().
@@ -256,11 +266,10 @@ function convertQueryStringToPattern(queryString) {
   var trimmedString = queryString.trim();
   // Escape for RegExp safety.
   var escString = Pattern.escape(trimmedString);
-  // Replace spaces between words with match pattern.
-  var preppedString = escString.replace(/\s+/, '[^\\s]* ');
-  // Create a RegExp pattern object via Pattern lib. Match globally and
-  // case-insentitively.
-  return Pattern(preppedString, 'i');
+
+  var escWords = escString.split(/\s+/);
+  var patternString = fold(escWords, foldWordsToLogicalAndPatternString, '');
+  return fold(patternString, convertStringToCaseInsensitivePattern);
 }
 
 function createActionArticle(title, subtitle, className) {
@@ -578,7 +587,7 @@ if(navigator.mozApps) {
     btn.addEventListener('click', function() {
         navigator.mozApps.install(location.href + 'manifest.webapp');
     });
-    
+
     var req = navigator.mozApps.getSelf();
     req.onsuccess = function() {
         if(!req.result) {
